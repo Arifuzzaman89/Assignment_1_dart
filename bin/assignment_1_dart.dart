a76e563f02a007b5d67649b255667010932790e4
xaman <Xamanarif189@gmail.com>
2021-12-02 20:04:23 +0600

    QuesNo2 done

--- bin/assignment_1_dart.dart
+++ bin/assignment_1_dart.dart
@@ -1,12 +1,21 @@
 void main(){
 
   questionNo1();
+  questionNo2();
 }
 //Question 1 01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
 
 void questionNo1(){
   print('Hello');
   print('Arifuzzaman\nLovelu');
+}
+//02. Write a Dart program to print the sum of two numbers.
+
+void questionNo2(){
+  int a=10;
+  int b=20;
+  int c=a+b;
+  print(c);
 }
 
 
