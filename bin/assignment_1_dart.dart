a01e7970e121fbdbc707af47d9fb0929cafaa2cd
xaman <Xamanarif189@gmail.com>
2021-12-02 19:59:45 +0600

    QuesNo1 done

--- bin/assignment_1_dart.dart
+++ bin/assignment_1_dart.dart
@@ -1,4 +1,12 @@
 void main(){
 
+  questionNo1();
 }
-//Question 1
+//Question 1 01. Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
+
+void questionNo1(){
+  print('Hello');
+  print('Arifuzzaman\nLovelu');
+}
+
+
